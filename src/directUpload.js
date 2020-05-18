16b7317962f52b83ec10372a96a3bac016a4d683
Kaleb Portilho <KalebPortillo@users.noreply.github.com>
2020-05-18 15:20:27 -0300

    add try catch inside promise

--- src/directUpload.js
+++ src/directUpload.js
@@ -3,55 +3,61 @@ import createBlobRecord from './createBlobRecord';
 
 let id = 0
 
-export default ({ directUploadsUrl, file }, onStatusChange) => {
-  const taskId = ++id;
-  let canceled = false;
-  let task;
+export default ({ directUploadsUrl, file }, onStatusChange = () => {}) => {
+  const taskId = ++id
+  let canceled = false
+  let task
 
   const handleCancel = () => {
-    if (!task) { return; }
+    if (!task) {
+      return
+    }
 
-    canceled = true;
-    task.cancel();
+    canceled = true
+    task.cancel()
   }
 
-  const handleStatusUpdate = (data) => {
-    onStatusChange({ ...data, id: taskId, cancel: handleCancel, file });
+  const handleStatusUpdate = data => {
+    onStatusChange({ ...data, id: taskId, cancel: handleCancel, file })
   }
 
-  handleStatusUpdate({ status: 'waiting' });
+  handleStatusUpdate({ status: 'waiting' })
 
   return new Promise(async (resolve, reject) => {
-    const blobData = await createBlobRecord({ directUploadsUrl, file });
-    const { url, headers } = blobData.direct_upload;
+    try {
+      const blobData = await createBlobRecord({ directUploadsUrl, file })
+      const { url, headers } = blobData.direct_upload
 
-    const fileData = RNFetchBlob.wrap(file.path);
+      const fileData = RNFetchBlob.wrap(file.path)
 
-    task = RNFetchBlob.fetch('PUT', url, headers, fileData)
+      task = RNFetchBlob.fetch('PUT', url, headers, fileData)
 
-    task
-      .uploadProgress({ interval: 250 }, (count, total) => {
-        const progress = (count / total) * 100
-        handleStatusUpdate({ status: 'progress', progress, total, count });
-      })
-      .then((resp) => {
-        const status = resp.info().status;
-        if (status >= 200 && status < 400) {
-          handleStatusUpdate({ status: 'finished' });
-        } else {
-          handleStatusUpdate({ status: 'error' });
-        }
+      task
+        .uploadProgress({ interval: 250 }, (count, total) => {
+          const progress = (count / total) * 100
+          handleStatusUpdate({ status: 'progress', progress, total, count })
+        })
+        .then(resp => {
+          const status = resp.info().status
+          if (status >= 200 && status < 400) {
+            handleStatusUpdate({ status: 'finished' })
+          } else {
+            handleStatusUpdate({ status: 'error' })
+          }
 
-        resolve(blobData.signed_id)
-      })
-      .catch((err) => {
-        if (canceled) {
-          handleStatusUpdate({ status: 'canceled' });
-        } else {
-          handleStatusUpdate({ status: 'error' });
-        }
+          resolve(blobData.signed_id)
+        })
+        .catch(err => {
+          if (canceled) {
+            handleStatusUpdate({ status: 'canceled' })
+          } else {
+            handleStatusUpdate({ status: 'error' })
+          }
 
-        resolve()
-      })
+          resolve()
+        })
+    } catch (e) {
+      reject(e)
+    }
   })
 }
