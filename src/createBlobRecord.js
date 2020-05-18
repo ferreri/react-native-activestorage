87e14b700cbbb225846c87fe62ac87b36ba3965a
Kaleb Portilho <KalebPortillo@users.noreply.github.com>
2020-05-18 17:07:25 -0300

    add return promise

--- src/createBlobRecord.js
+++ src/createBlobRecord.js
@@ -1,27 +1,29 @@
 import getChecksum from './checksum';
 
 export default async ({ directUploadsUrl = '/rails/active_storage/direct_uploads', file }) => {
-  const checksum = await getChecksum({ path: file.path });
+  return new Promise(async (resolve, reject) => {
+    const checksum = await getChecksum({ path: file.path })
 
-  const params = {
-    filename: file.name,
-    content_type: file.type || 'image/jpeg',
-    byte_size: file.size,
-    checksum,
-  }
+    const params = {
+      filename: file.name,
+      content_type: file.type || 'image/jpeg',
+      byte_size: file.size,
+      checksum
+    }
 
-  if (file.metadata) {
-    params.metadata = file.metadata;
-  }
+    if (file.metadata) {
+      params.metadata = file.metadata
+    }
 
-  try {
-    const response = await fetch(directUploadsUrl, {
-      method: 'POST',
-      body: JSON.stringify({blob: params}),
-      headers: {'Content-Type': 'application/json'},
-    })
-    resolve(response.json())
-  } catch (e) {
-    reject(e)
-  }
+    try {
+      const response = await fetch(directUploadsUrl, {
+        method: 'POST',
+        body: JSON.stringify({ blob: params }),
+        headers: { 'Content-Type': 'application/json' }
+      })
+      resolve(response.json())
+    } catch (e) {
+      reject(e)
+    }
+  })
 }
